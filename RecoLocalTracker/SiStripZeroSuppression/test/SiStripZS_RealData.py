3583536ab88db4bb6109d895fd2c2642c16f6e28
Ivan <icali@cern.ch>
2018-03-14 15:13:05 +0100

    adding hybryd generathor method

--- RecoLocalTracker/SiStripZeroSuppression/test/SiStripZS_RealData.py
+++ RecoLocalTracker/SiStripZeroSuppression/test/SiStripZS_RealData.py
@@ -1,17 +1,11 @@
-# Auto generated configuration file
-# using: 
-# Revision: 1.222.2.6 
-# Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
-# with command line options: hiRecoDM -n 10 --scenario HeavyIons -s RAW2DIGI,L1Reco,RECO --processName TEST --datatier GEN-SIM-RECO --eventcontent FEVTDEBUG --customise SimGeneral.DataMixingModule.DataMixer_DataConditions_3_8_X_data2010 --cust_function customise --geometry DB --filein file:DMRawSimOnReco_DIGI2RAW.root --fileout hiRecoDM_RECO.root --conditions FrontierConditions_GlobalTag,MC_38Y_V12::All --no_exec
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process('TEST')
+process = cms.Process('TEST1')
 
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
-#process.load('Configuration.StandardSequences.MixingNoPileUp_cff')
 process.load('Configuration.StandardSequences.GeometryDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_cff')
 process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
@@ -22,90 +16,106 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load('Configuration.EventContent.EventContentHeavyIons_cff')
 
 process.configurationMetadata = cms.untracked.PSet(
-    annotation = cms.untracked.string('hiRecoDM nevts:10'),
+    annotation = cms.untracked.string('hiRecoDM nevts:2'),
     name = cms.untracked.string('PyReleaseValidation')
 )
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10)
+    input = cms.untracked.int32(20)
 )
-process.options = cms.untracked.PSet(
-    wantSummary = cms.untracked.bool(True)
-)
-process.Timing = cms.Service("Timing")
-#process.MessageLogger.cerr.threshold = cms.untracked.string("DEBUG")
-#process.MessageLogger.categories+=cms.vstring("SiStripFedCMExtractor","SiStripProcessedRawDigiSkimProducer")
 
 # Input source
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-    #'/store/express/Run2010B/ExpressPhysics/FEVT/Express-v2/000/146/417/10F981FF-5EC6-DF11-9657-0030486733B4.root'
-    #'file:../testGenSimOnReco/SingleZmumu_MatchVertexDM_DIGI2RAW.root'
-    #'file:DMRawSimOnReco_DIGI2RAW.root'
-    #'file:DMRawSimOnReco_DIGI2RAW.root'
-	'/store/data/Run2010B/HeavyIonTest/RAW/v1/000/146/421/E6B24CF0-5EC6-DF11-B52D-00304879FC6C.root'
-    )
+    #'file:/data/abaty/VirginRaw_CentralitySkims/VirginRAW_2010_HICorePhysics_SKIM_Cent_0_5_1.root',
+    #'file:/data/abaty/HLT_Emulated_2010Data/outputHIPhysicsVirginRaw.root'
+    #'file:/data/abaty/VirginRaw_CentralitySkims/VirginRAW_2010_HICorePhysics_SKIM_Cent_0_5_10.root',
+    #'/store/hidata/HIRun2015/HITrackerVirginRaw/RAW/v1/000/263/400/00000/40322926-4AA3-E511-95F7-02163E0146A8.root'
+    'file:inputVR.root'
+   )
 )
 
-
 #process.source = cms.Source("NewEventStreamFileReader",
-#fileNames = cms.untracked.vstring('file:Data.00148514.0021.A.storageManager.01.0000.dat'
+#fileNames = cms.untracked.vstring('root://eoscms.cern.ch//store/t0streamer/Data/HIPhysicsVirginRaw/000/262/296/run262296_ls0223_streamHIPhysicsVirginRaw_StorageManager.dat'
 #)
 #)
-
 
 # Output definition
 process.RECOoutput = cms.OutputModule("PoolOutputModule",
     splitLevel = cms.untracked.int32(0),
-    #outputCommands = process.RECOEventContent.outputCommands,
-    fileName = cms.untracked.string('hiReco_E6B24CF0-5EC6-DF11-B52D-00304879FC6C_10ev.root'),
-	#fileName = cms.untracked.string('test.root'),
+    fileName = cms.untracked.string('RECO.root'),
     dataset = cms.untracked.PSet(
         filterName = cms.untracked.string(''),
         dataTier = cms.untracked.string('GEN-SIM-RECO')
     )
 )
 
-# Additional output definition
-#process.RECOoutput.outputCommands.extend(['keep *_siStripProcessedRawDigisSkim_*_*',
-#                                               'keep *_*_APVCM_*'])
 
-# Other statements
-process.GlobalTag.globaltag = 'GR_R_39X_V1::All'
-#process.GlobalTag.globaltag = 'MC_39Y_V4::All'
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = '75X_dataRun2_HLTHI_v4'
 
 ## Offline Silicon Tracker Zero Suppression
+from RecoLocalTracker.SiStripZeroSuppression.DefaultAlgorithms_cff import *
 process.siStripZeroSuppression.Algorithms.PedestalSubtractionFedMode = cms.bool(False)
 process.siStripZeroSuppression.Algorithms.CommonModeNoiseSubtractionMode = cms.string("IteratedMedian")
+#process.siStripZeroSuppression.Algorithms.APVInspectMode = cms.string("Null")
 process.siStripZeroSuppression.doAPVRestore = cms.bool(True)
 process.siStripZeroSuppression.produceRawDigis = cms.bool(True)
 process.siStripZeroSuppression.produceCalculatedBaseline = cms.bool(True)
+process.siStripZeroSuppression.produceBaselinePoints = cms.bool(True)
 process.siStripZeroSuppression.storeCM = cms.bool(True)
 process.siStripZeroSuppression.storeInZScollBadAPV = cms.bool(True)
 
 
-    
+process.TFileService = cms.Service("TFileService",
+        fileName=cms.string("Baselines.root"))
 
+process.baselineAna = cms.EDAnalyzer("SiStripBaselineAnalyzer",
+        Algorithms = DefaultAlgorithms,
+        srcBaseline =  cms.InputTag('siStripZeroSuppression','BADAPVBASELINEVirginRaw'),
+        srcBaselinePoints =  cms.InputTag('siStripZeroSuppression','BADAPVBASELINEPOINTSVirginRaw'),
+        srcAPVCM  =  cms.InputTag('siStripZeroSuppression','APVCMVirginRaw'),
+        #srcProcessedRawDigi =  cms.InputTag('siStripZeroSuppression','VirginRaw'),
+        srcProcessedRawDigi =  cms.InputTag('siStripDigis','VirginRaw'),
+        srcClusters =  cms.InputTag('siStripClusters',''),
+        nModuletoDisplay = cms.uint32(1000),
+        plotClusters = cms.bool(True),
+        plotBaseline = cms.bool(True),
+        plotBaselinePoints = cms.bool(True),
+        plotRawDigi     = cms.bool(True),
+        plotAPVCM   = cms.bool(True),
+        plotPedestals = cms.bool(False)
+)
 
+process.moddedZS = process.siStripZeroSuppression.clone()
+process.moddedZS.Algorithms.APVInspectMode = cms.string("BaselineFollower")
+process.moddedZS.Algorithms.APVRestoreMode = cms.string("BaselineFollower")
+process.moddedZS.Algorithms.consecThreshold = cms.uint32(9)#just for testing
 
-process.TimerService = cms.Service("TimerService", useCPUtime = cms.untracked.bool(True) # set to false for wall-clock-time
-)								  
+process.moddedClust = process.siStripClusters.clone()
+process.moddedClust.DigiProducersList = cms.VInputTag(
+    cms.InputTag('siStripDigis','ZeroSuppressed'),
+    cms.InputTag('moddedZS','VirginRaw'),
+    cms.InputTag('moddedZS','ProcessedRaw'),
+    cms.InputTag('moddedZS','ScopeMode'))
+
+process.moddedbaselineAna = process.baselineAna.clone()
+process.moddedbaselineAna.srcBaseline = cms.InputTag('moddedZS','BADAPVBASELINEVirginRaw')
+process.moddedbaselineAna.srcBaselinePoints = cms.InputTag('moddedZS','BADAPVBASELINEPOINTSVirginRaw')
+process.moddedbaselineAna.srcAPVCM = cms.InputTag('moddedZS','APVCMVirginRaw')
+process.moddedbaselineAna.srcProcessedRawDigi = cms.InputTag('moddedZS','VirginRaw')
+process.moddedbaselineAna.srcClusters = cms.InputTag('moddedClust','')
+
+from RecoLocalTracker.SiStripZeroSuppression.SiStripBaselineComparitor_cfi import *
+process.clusterMatching = cms.EDAnalyzer("SiStripBaselineComparitor",
+    srcClusters =  cms.InputTag('siStripClusters',''),
+    srcClusters2 =  cms.InputTag('moddedClust','')
+)
 								  
 # Path and EndPath definitions
 process.raw2digi_step = cms.Path(process.siStripDigis)
-process.reconstruction_step = cms.Path(process.trackerlocalreco)
-#process.reconstruction_step = cms.Path(process.striptrackerlocalreco)
-process.endjob_step = cms.Path(process.endOfProcess)
+process.reconstruction_step = cms.Path(process.striptrackerlocalreco+process.moddedZS+process.moddedClust+process.baselineAna+process.moddedbaselineAna+process.clusterMatching)
+#process.endjob_step = cms.EndPath(process.endOfProcess)
 process.RECOoutput_step = cms.EndPath(process.RECOoutput)
-#process.Timer_step = cms.Path(process.myTimer)
 
 # Schedule definition
-#process.schedule = cms.Schedule(process.raw2digi_step,process.reconstruction_step,process.hipmonitor_step, process.RECOoutput_step)
 process.schedule = cms.Schedule(process.raw2digi_step,process.reconstruction_step, process.RECOoutput_step)
-# customisation of the process
-
-
-
-
-
-
-# End of customisation functions
