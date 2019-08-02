d97a730210bbb8e6ff422b23608939cec2a24079
Florian Bury <florian.joel.j.bury@cern.ch>
2019-08-02 14:51:10 +0200

    Removed raw2digi in Sequence

--- RecoLocalTracker/SiStripZeroSuppression/test/SiStripZS_EmulateHybrid_Parallel.py
+++ RecoLocalTracker/SiStripZeroSuppression/test/SiStripZS_EmulateHybrid_Parallel.py
@@ -156,7 +156,8 @@ process.endjob_step = cms.EndPath(process.endOfProcess)
 process.RAWoutput_step = cms.EndPath(process.RAWoutput)
 
 # Schedule definition
-process.schedule = cms.Schedule(process.raw2digi_step,process.DigiToRawZS,process.endjob_step,process.RAWoutput_step)
+#process.schedule = cms.Schedule(process.raw2digi_step,process.DigiToRawZS,process.endjob_step,process.RAWoutput_step)
+process.schedule = cms.Schedule(process.DigiToRawZS,process.endjob_step,process.RAWoutput_step)
 from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
 associatePatAlgosToolsTask(process)
 
