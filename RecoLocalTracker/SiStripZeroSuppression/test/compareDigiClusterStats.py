bb17d3a949330c59a09dd1291fed6c8125ea98f0
Florian Bury <florian.joel.j.bury@cern.ch>
2019-08-05 14:07:22 +0200

    Added the scirpt to plot the Digi and clusters scripts

---
+++ RecoLocalTracker/SiStripZeroSuppression/test/compareDigiClusterStats.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python2
+"""
+A few quick (ZS) digi and cluster statistics comparison plots
+"""
+__author__ = "Pieter David <pieter.david@uclouvain.be>"
+__date__ = "22 September 2018"
+
+import os.path
+import ROOT
+from matplotlib import pyplot as plt
+import tdrstyle
+
+ROOT.gROOT.SetBatch(True)
+tdrstyle.setTDRStyle()
+
+def addOverflows(histo):
+    histo.SetBinContent(1, histo.GetBinContent(0)+histo.GetBinContent(1))
+    nB = histo.GetNbinsX()
+    histo.SetBinContent(nB, histo.GetBinContent(nB)+histo.GetBinContent(nB+1))
+
+if __name__ == "__main__":
+    name = "combinedRuns.root"
+    hFile = ROOT.TFile.Open(name, "READ")
+    refSuffix = "B" # Classic ZS
+    newSuffix = "A" # Hybrid ZS
+    outpath = "comparisonPlots"
+    histoList = ["digiStatDiff/nDigis", "clusterStatDiff/nClus",
+            "clusterStatDiff/clusCharge", "clusterStatDiff/clusWidth", "clusterStatDiff/clusBary", "clusterStatDiff/clusVar"]
+    titleList = ["Number of digis", "Number of clusters",
+                "Cluster charge", "Cluster Width", "Cluster barycenter", "Cluster variance"]
+    for hName,hTitle in zip(histoList,titleList):
+        # Get histos #
+        hName_n = hName.replace("/", "_")
+        hRef = hFile.Get(hName+refSuffix)
+        hNew = hFile.Get(hName+newSuffix)
+        addOverflows(hRef)
+        addOverflows(hNew)
+
+        # Make Ratio #
+        hRatio = hNew.Clone(hName+"Ratio")
+        hRatio.Sumw2()
+        hRatio.Divide(hRef)
+
+        # Plot the Ratio #
+        canvas = ROOT.TCanvas('c1','c1',800,700)
+        pad1 = ROOT.TPad("pad1", "pad1", 0, 0.0, 1, 1.0)
+        pad1.SetBottomMargin(0.32)
+        pad1.SetGridx()
+        pad1.Draw()
+        pad1.cd()
+        pad1.SetLogy()
+        hRef.SetStats(0)
+        hRef.SetTitle(hTitle)
+        hRef.SetTitleSize(14)
+        hRef.Draw()
+        hNew.Draw("same")
+        legend = ROOT.TLegend(0.65,0.7,0.85,0.83)
+        legend.SetHeader("Legend","C")
+        legend.AddEntry(hRef,"Classic ZS","l")
+        legend.AddEntry(hNew,"Hybrid ZS","l")
+        legend.Draw()
+
+        pad2 = ROOT.TPad("pad2", "pad2", 0, 0.0, 1, 0.26)
+        pad2.SetTopMargin(0)
+        pad2.SetBottomMargin(0.4)
+        pad2.SetGridx()
+        pad2.Draw()
+        pad2.cd() 
+        hRatio.Draw()
+        hRatio.SetLineColor(ROOT.kBlack)
+        hRatio.SetMinimum(0.5)
+        hRatio.SetMaximum(1.5)
+        hRatio.SetStats(0)                                                                                                                                                                              
+        hRatio.SetMarkerStyle(21)
+        hRatio.Draw("ep")
+
+        hRef.SetLineColor(ROOT.kBlue+1)
+        hRef.SetLineWidth(2)
+        max_y = max(hRef.GetMaximum(),hNew.GetMaximum())
+        hRef.SetMaximum(max_y*1.1)
+        hRef.GetYaxis().SetNdivisions(505)                                                                                                                                                           
+        hRef.GetYaxis().SetTitleSize(14)
+        hRef.GetYaxis().SetTitleFont(43)
+        hRef.GetYaxis().SetTitleOffset(1.8)
+
+        hNew.SetLineColor(ROOT.kRed)
+        hNew.SetLineWidth(2)
+
+        hRatio.SetTitle("")
+
+        hRatio.GetYaxis().SetTitle("Ratio")
+        hRatio.GetYaxis().SetNdivisions(505)
+        hRatio.GetYaxis().SetTitleSize(14)
+        hRatio.GetYaxis().SetTitleFont(43)
+        hRatio.GetYaxis().SetTitleOffset(1.8)
+        hRatio.GetYaxis().SetLabelFont(43)
+        hRatio.GetYaxis().SetLabelSize(15)
+
+        hRatio.GetXaxis().SetNdivisions(510)
+        hRatio.GetXaxis().SetTitleSize(14)
+        hRatio.GetXaxis().SetTitleFont(43)
+        hRatio.GetXaxis().SetTitleOffset(4.)
+        hRatio.GetXaxis().SetLabelFont(43)
+        hRatio.GetXaxis().SetLabelSize(15)
+    
+        canvas.Print(name.replace('.root',hName_n+'.pdf'))
+        ##
+        hDiff = hFile.Get(hName+"Diff")
+        hRelDiff = hFile.Get(hName+"RelDiff")
+        ##
+        canvas.Clear()
+
+        if hDiff:
+            addOverflows(hDiff)
+            hDiff.SetTitle(hTitle+' difference')
+            hDiff.Draw()
+            canvas.Print(name.replace('.root',hName_n+'_diff.pdf'))
+            canvas.Clear()
+        if hRelDiff: 
+            addOverflows(hRelDiff)
+            hDiff.SetTitle(hTitle+' relative difference')
+            hDiff.Draw()
+            canvas.Print(name.replace('.root',hName_n+'_reldiff.pdf'))
+            canvas.Clear()
+       
