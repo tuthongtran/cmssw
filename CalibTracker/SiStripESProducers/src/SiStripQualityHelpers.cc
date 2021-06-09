74ffcef5ef54807b92ae1635210c4d255fe8212f
Pieter David <pieter.david@cern.ch>
2021-06-09 17:58:32 +0200

    Update merging of bad components from FED errors to also read from legacy DQM files

--- CalibTracker/SiStripESProducers/src/SiStripQualityHelpers.cc
+++ CalibTracker/SiStripESProducers/src/SiStripQualityHelpers.cc
@@ -1,7 +1,7 @@
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "TFile.h"
+#include "TKey.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
-#include "CalibFormats/SiStripObjects/interface/SiStripQuality.h"
-#include "CondFormats/SiStripObjects/interface/SiStripFedCabling.h"
+#include "CondFormats/DataRecord/interface/SiStripFedCablingRcd.h"
 
 #include "CalibTracker/SiStripESProducers/interface/SiStripQualityHelpers.h"
 
@@ -10,9 +10,30 @@ using dqm::harvesting::MonitorElement;
 
 namespace {
 
+  void fillFedBadChannelMap(const TH2F* th2,
+                            float entries,
+                            float cutoff,
+                            const SiStripFedCabling& fedCabling,
+                            std::map<uint32_t, std::set<int>>& detectorMap) {
+    for (uint16_t i = 1; i < th2->GetNbinsY() + 1; ++i) {
+      for (uint16_t j = 1; j < th2->GetNbinsX() + 1; ++j) {
+        if (th2->GetBinContent(j, i) > cutoff * entries) {
+          const uint16_t fId = th2->GetYaxis()->GetBinLowEdge(i);
+          edm::LogInfo("SiStripBadModuleFedErrService")
+              << " [SiStripBadModuleFedErrService::getFedBadChannelList] :: FedId & Channel " << fId << "  "
+              << th2->GetXaxis()->GetBinLowEdge(j);
+          const uint16_t fChan = uint16_t(th2->GetXaxis()->GetBinLowEdge(j)) / 2;
+          if (!((fId == 9999) && (fChan == 9999))) {
+            const auto channel = fedCabling.fedConnection(fId, fChan);
+            detectorMap[channel.detId()].insert(channel.apvPairNumber());
+          }
+        }
+      }
+    }
+  }
+
   float getProcessedEvents(DQMStore::IGetter& dqmStore) {
     dqmStore.cd();
-
     const std::string dname{"SiStrip/ReadoutView"};
     const std::string hpath{dname + "/nTotalBadActiveChannels"};
     if (dqmStore.dirExists(dname)) {
@@ -23,28 +44,106 @@ namespace {
     return 0;
   }
 
-  std::vector<std::pair<uint16_t, uint16_t>> getFedBadChannelList(DQMStore::IGetter& dqmStore,
-                                                                  const MonitorElement* me,
-                                                                  float cutoff) {
-    std::vector<std::pair<uint16_t, uint16_t>> ret;
-    if (me->kind() == MonitorElement::Kind::TH2F) {
-      TH2F* th2 = me->getTH2F();
-      float entries = getProcessedEvents(dqmStore);
-      if (!entries)
-        entries = th2->GetBinContent(th2->GetMaximumBin());
-      for (uint16_t i = 1; i < th2->GetNbinsY() + 1; ++i) {
-        for (uint16_t j = 1; j < th2->GetNbinsX() + 1; ++j) {
-          if (th2->GetBinContent(j, i) > cutoff * entries) {
-            edm::LogInfo("SiStripBadModuleFedErrService")
-                << " [SiStripBadModuleFedErrService::getFedBadChannelList] :: FedId & Channel "
-                << th2->GetYaxis()->GetBinLowEdge(i) << "  " << th2->GetXaxis()->GetBinLowEdge(j);
-            ret.push_back(
-                std::pair<uint16_t, uint16_t>(th2->GetYaxis()->GetBinLowEdge(i), th2->GetXaxis()->GetBinLowEdge(j)));
-          }
+  std::map<uint32_t, std::set<int>> getBadChannelDetectorMap(DQMStore::IGetter& dqmStore,
+                                                             const SiStripFedCabling& fedCabling,
+                                                             float cutoff) {
+    std::map<uint32_t, std::set<int>> detectorMap;
+    dqmStore.cd();
+    const std::string dname{"SiStrip/ReadoutView"};
+    const std::string hpath{dname + "/FedIdVsApvId"};
+    if (dqmStore.dirExists(dname)) {
+      MonitorElement* me = dqmStore.get(hpath);
+      if (me && (me->kind() == MonitorElement::Kind::TH2F)) {
+        TH2F* th2 = me->getTH2F();
+        float entries = getProcessedEvents(dqmStore);
+        if (!entries) {
+          edm::LogWarning("SiStripBadModuleFedErrService") << "Normalising to the largest bin";
+          entries = th2->GetBinContent(th2->GetMaximumBin());
         }
+        fillFedBadChannelMap(th2, entries, cutoff, fedCabling, detectorMap);
+      } else {
+        edm::LogError("SiStripBadModuleFedErrService") << "Could not find SiStrip/ReadoutView/FedIdVsApvId";
+      }
+    } else {
+      edm::LogError("SiStripBadModuleFedErrService") << "Could not find SiStrip/ReadoutView";
+    }
+    return detectorMap;
+  }
+
+  TDirectoryFile* checkAndGetSubdir(TDirectoryFile* tdir, const std::vector<std::string>& path) {
+    for (const auto& subName : path) {
+      tdir = tdir->Get<TDirectoryFile>(subName.c_str());
+      if (!tdir) {
+        break;
       }
     }
-    return ret;
+    return tdir;
+  }
+
+  std::map<uint32_t, std::set<int>> getBadChannelDetectorMap(TFile* legacyDQMTDirFile,
+                                                             unsigned int runNumber,
+                                                             const SiStripFedCabling& fedCabling,
+                                                             float cutoff) {
+    std::map<uint32_t, std::set<int>> detectorMap;
+    const auto stripROVDir = checkAndGetSubdir(
+        legacyDQMTDirFile, {"DQMData", "Run " + std::to_string(runNumber), "SiStrip", "Run summary", "ReadoutView"});
+    if (!stripROVDir) {
+      edm::LogError("SiStripBadModuleFedErrService")
+          << "Could not find SiStrip/ReadoutView directory in " << legacyDQMTDirFile->GetName() << " with run number "
+          << runNumber;
+    } else {
+      const auto th2 = stripROVDir->Get<TH2F>("FedIdVsApvId");
+      if (!th2) {
+        edm::LogError("SiStripBadModuleFedErrService")
+            << "Could not find SiStrip/ReadoutView/FedIdVsApvId in " << legacyDQMTDirFile->GetName();
+      } else {
+        float entries = 0.;
+        const auto nActChan = stripROVDir->Get<TH1F>("nTotalBadActiveChannels");
+        if (nActChan) {
+          entries = nActChan->GetEntries();
+        }
+        if (!entries) {
+          edm::LogWarning("SiStripBadModuleFedErrService") << "Normalising to the largest bin";
+          entries = th2->GetBinContent(th2->GetMaximumBin());
+        }
+        fillFedBadChannelMap(th2, entries, cutoff, fedCabling, detectorMap);
+      }
+    }
+    return detectorMap;
+  }
+
+  void fillQuality(SiStripQuality* quality, const std::map<uint32_t, std::set<int>>& detectorMap) {
+    for (const auto& detElm : detectorMap) {  // pair(detId, pairs)
+      SiStripQuality::InputVector theSiStripVector;
+      unsigned short firstBadStrip{0};
+      unsigned short fNconsecutiveBadStrips{0};
+      int last_pair = -1;
+      for (const auto pair : detElm.second) {
+        if (last_pair == -1) {
+          firstBadStrip = pair * 128 * 2;
+          fNconsecutiveBadStrips = 128 * 2;
+        } else if (pair - last_pair > 1) {
+          theSiStripVector.push_back(quality->encode(firstBadStrip, fNconsecutiveBadStrips));
+          firstBadStrip = pair * 128 * 2;
+          fNconsecutiveBadStrips = 128 * 2;
+        } else {
+          fNconsecutiveBadStrips += 128 * 2;
+        }
+        last_pair = pair;
+      }
+      unsigned int theBadStripRange = quality->encode(firstBadStrip, fNconsecutiveBadStrips);
+      theSiStripVector.push_back(theBadStripRange);
+
+      edm::LogInfo("SiStripBadModuleFedErrService")
+          << " SiStripBadModuleFedErrService::readBadComponentsFromFed "
+          << " detid " << detElm.first << " firstBadStrip " << firstBadStrip << " NconsecutiveBadStrips "
+          << fNconsecutiveBadStrips << " packed integer " << std::hex << theBadStripRange << std::dec;
+
+      if (!quality->put(detElm.first, SiStripBadStrip::Range{theSiStripVector.begin(), theSiStripVector.end()})) {
+        edm::LogError("SiStripBadModuleFedErrService")
+            << "[SiStripBadModuleFedErrService::readBadComponentsFromFed] detid already exists";
+      }
+    }
   }
 
 }  // namespace
@@ -53,56 +152,67 @@ std::unique_ptr<SiStripQuality> sistrip::badStripFromFedErr(DQMStore::IGetter& d
                                                             const SiStripFedCabling& fedCabling,
                                                             float cutoff) {
   auto quality = std::make_unique<SiStripQuality>();
-  dqmStore.cd();
-  const std::string dname{"SiStrip/ReadoutView"};
-  const std::string hpath{dname + "/FedIdVsApvId"};
-  if (dqmStore.dirExists(dname)) {
-    MonitorElement* me = dqmStore.get(hpath);
-    if (me) {
-      std::map<uint32_t, std::set<int>> detectorMap;
-      for (const auto& elm : getFedBadChannelList(dqmStore, me, cutoff)) {
-        const uint16_t fId = elm.first;
-        const uint16_t fChan = elm.second / 2;
-        if ((fId == 9999) && (fChan == 9999))
-          continue;
-
-        FedChannelConnection channel = fedCabling.fedConnection(fId, fChan);
-        detectorMap[channel.detId()].insert(channel.apvPairNumber());
-      }
-
-      for (const auto& detElm : detectorMap) {  // pair(detId, pairs)
-        SiStripQuality::InputVector theSiStripVector;
-        unsigned short firstBadStrip{0};
-        unsigned short fNconsecutiveBadStrips{0};
-        int last_pair = -1;
-        for (const auto pair : detElm.second) {
-          if (last_pair == -1) {
-            firstBadStrip = pair * 128 * 2;
-            fNconsecutiveBadStrips = 128 * 2;
-          } else if (pair - last_pair > 1) {
-            theSiStripVector.push_back(quality->encode(firstBadStrip, fNconsecutiveBadStrips));
-            firstBadStrip = pair * 128 * 2;
-            fNconsecutiveBadStrips = 128 * 2;
-          } else {
-            fNconsecutiveBadStrips += 128 * 2;
-          }
-          last_pair = pair;
-        }
-        unsigned int theBadStripRange = quality->encode(firstBadStrip, fNconsecutiveBadStrips);
-        theSiStripVector.push_back(theBadStripRange);
-
-        edm::LogInfo("SiStripBadModuleFedErrService")
-            << " SiStripBadModuleFedErrService::readBadComponentsFromFed "
-            << " detid " << detElm.first << " firstBadStrip " << firstBadStrip << " NconsecutiveBadStrips "
-            << fNconsecutiveBadStrips << " packed integer " << std::hex << theBadStripRange << std::dec;
-
-        if (!quality->put(detElm.first, SiStripBadStrip::Range{theSiStripVector.begin(), theSiStripVector.end()})) {
-          edm::LogError("SiStripBadModuleFedErrService")
-              << "[SiStripBadModuleFedErrService::readBadComponentsFromFed] detid already exists";
-        }
-      }
-      quality->cleanUp();
-    }
+  auto detectorMap = getBadChannelDetectorMap(dqmStore, fedCabling, cutoff);
+  if (!detectorMap.empty()) {
+    fillQuality(quality.get(), detectorMap);
+    quality->cleanUp();
+  } else {
+    edm::LogWarning("SiStripBadModuleFedErrService") << "Empty bad channel map from FED errors";
   }
   return quality;
+}
+
+std::unique_ptr<SiStripQuality> sistrip::badStripFromFedErrLegacyDQMFile(const std::string& fileName,
+                                                                         unsigned int runNumber,
+                                                                         const SiStripFedCabling& fedCabling,
+                                                                         float cutoff) {
+  auto quality = std::make_unique<SiStripQuality>();
+  auto tdirFile = TFile::Open(fileName.c_str());
+  auto detectorMap = getBadChannelDetectorMap(tdirFile, runNumber, fedCabling, cutoff);
+  if (!detectorMap.empty()) {
+    fillQuality(quality.get(), detectorMap);
+    quality->cleanUp();
+  } else {
+    edm::LogWarning("SiStripBadModuleFedErrService") << "Empty bad channel map from FED errors";
+  }
+  return quality;
+}
+
+bool sistrip::MergedQualityWithFromFedErr::endRun(const edm::EventSetup& iSetup) {
+  if (stripQualityWatcher_.check(iSetup)) {
+    if (keepCopy_) {
+      mergedQuality_ = std::make_unique<SiStripQuality>(iSetup.getData(stripQualityToken_));
+      if (addBadCompFromFedErr_) {
+        fedCabling_ = std::make_unique<SiStripFedCabling>(iSetup.getData(fedCablingToken_));
+      }
+      merged_ = false;
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const SiStripQuality& sistrip::MergedQualityWithFromFedErr::getMergedQuality(
+    dqm::harvesting::DQMStore::IGetter& getter) {
+  if (!merged_) {
+    if (addBadCompFromFedErr_) {
+      std::unique_ptr<SiStripQuality> fedErrQuality{};
+      if (fedErrLegacyFile_.empty()) {
+        edm::LogInfo("SiStripQuality") << "Adding bad components from FED errors in DQM store";
+        fedErrQuality = sistrip::badStripFromFedErr(getter, *fedCabling_, fedErrCutoff_);
+      } else {
+        edm::LogInfo("SiStripQuality") << "Adding bad components from FED errors in legacy DQM file "
+                                       << fedErrLegacyFile_;
+        fedErrQuality = sistrip::badStripFromFedErrLegacyDQMFile(
+            fedErrLegacyFile_, fedErrFileRunNumber_, *fedCabling_, fedErrCutoff_);
+      }
+      mergedQuality_->add(fedErrQuality.get());
+      mergedQuality_->cleanUp();
+      mergedQuality_->fillBadComponents();
+    }
+    merged_ = true;
+  }
+
+  return *mergedQuality_;
 }
--- CalibTracker/SiStripQuality/plugins/SiStripBadStripFromQualityDBWriter.cc
+++ CalibTracker/SiStripQuality/plugins/SiStripBadStripFromQualityDBWriter.cc
@@ -1,15 +1,12 @@
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/ESWatcher.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
 #include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
-#include "CalibTracker/Records/interface/SiStripQualityRcd.h"
-#include "CalibFormats/SiStripObjects/interface/SiStripQuality.h"
 #include "CondCore/DBOutputService/interface/PoolDBOutputService.h"
-#include "CondFormats/SiStripObjects/interface/SiStripFedCabling.h"
 #include "CalibTracker/SiStripESProducers/interface/SiStripQualityHelpers.h"
 #include "DQMServices/Core/interface/DQMEDHarvester.h"
+#include "CondFormats/SiStripObjects/interface/SiStripBadStrip.h"
 
 class SiStripBadStripFromQualityDBWriter : public DQMEDHarvester {
 public:
@@ -24,53 +21,30 @@ public:
 private:
   std::string rcdName_, openIOVAt_;
   uint32_t openIOVAtTime_;
-  bool addBadCompFromFedErr_;
-  float fedErrCutoff_;
-
-  edm::ESGetToken<SiStripQuality, SiStripQualityRcd> stripQualityToken_;
-  edm::ESGetToken<SiStripFedCabling, SiStripFedCablingRcd> fedCablingToken_;
-  edm::ESWatcher<SiStripQualityRcd> stripQualityWatcher_;
-  const SiStripFedCabling* fedCabling_;
-  const SiStripQuality* siStripQuality_;
+  sistrip::MergedQualityWithFromFedErr helper_;
 };
 
 SiStripBadStripFromQualityDBWriter::SiStripBadStripFromQualityDBWriter(const edm::ParameterSet& iConfig)
     : rcdName_{iConfig.getParameter<std::string>("record")},
       openIOVAt_{iConfig.getUntrackedParameter<std::string>("OpenIovAt", "beginOfTime")},
       openIOVAtTime_{iConfig.getUntrackedParameter<uint32_t>("OpenIovAtTime", 1)},
-      addBadCompFromFedErr_{iConfig.getUntrackedParameter<bool>("AddBadComponentsFromFedErrors", false)},
-      fedErrCutoff_{float(iConfig.getUntrackedParameter<double>("FedErrorBadComponentsCutoff", 0.8))},
-      stripQualityToken_{esConsumes<edm::Transition::EndRun>()},
-      fedCablingToken_{addBadCompFromFedErr_ ? decltype(fedCablingToken_){esConsumes<edm::Transition::EndRun>()}
-                                             : decltype(fedCablingToken_){}} {}
+      helper_{iConfig, consumesCollector(), true} {}
 
 void SiStripBadStripFromQualityDBWriter::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   edm::ParameterSetDescription desc;
   desc.add<std::string>("record", "");
   desc.addUntracked<std::string>("OpenIovAt", "beginOfTime");
   desc.addUntracked<uint32_t>("OpenIovAtTime", 1);
-  desc.addUntracked<bool>("AddBadComponentsFromFedErrors", false);
-  desc.addUntracked<double>("FedErrorBadComponentsCutoff", 0.8);
+  sistrip::MergedQualityWithFromFedErr::fillDescription(desc);
   descriptions.add("siStripBadStripFromQualityDBWriter", desc);
 }
 
 void SiStripBadStripFromQualityDBWriter::endRun(edm::Run const& /*run*/, edm::EventSetup const& iSetup) {
-  if (stripQualityWatcher_.check(iSetup)) {
-    siStripQuality_ = &iSetup.getData(stripQualityToken_);
-    if (addBadCompFromFedErr_) {
-      fedCabling_ = &iSetup.getData(fedCablingToken_);
-    }
-  }
+  helper_.endRun(iSetup);
 }
 
 void SiStripBadStripFromQualityDBWriter::dqmEndJob(DQMStore::IBooker& /*booker*/, DQMStore::IGetter& getter) {
-  auto mergedQuality = SiStripQuality(*siStripQuality_);
-  if (addBadCompFromFedErr_) {
-    auto fedErrQuality = sistrip::badStripFromFedErr(getter, *fedCabling_, fedErrCutoff_);
-    mergedQuality.add(fedErrQuality.get());
-    mergedQuality.cleanUp();
-    mergedQuality.fillBadComponents();
-  }
+  auto payload = std::make_unique<SiStripBadStrip>(helper_.getMergedQuality(getter));
   cond::Time_t time;
   edm::Service<cond::service::PoolDBOutputService> dbservice;
   if (dbservice.isAvailable()) {
@@ -81,7 +55,7 @@ void SiStripBadStripFromQualityDBWriter::dqmEndJob(DQMStore::IBooker& /*booker*/
     else
       time = openIOVAtTime_;
 
-    dbservice->writeOne(new SiStripBadStrip(mergedQuality), time, rcdName_);
+    dbservice->writeOne(payload.release(), time, rcdName_);
   } else {
     edm::LogError("SiStripBadStripFromQualityDBWriter") << "Service is unavailable" << std::endl;
   }
